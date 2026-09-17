592f39da3ab8b40c291000c5faaacb80b42d2b29
JanKaellito <jankylecluma@gmail.com>
2026-09-17 19:25:45 +0800

    Delete Project-2-main directory

--- Project-2-main/api/application_update_status.php
+++
@@ -1,7 +0,0 @@
-<?php
-require "config.php";
-$data = readJsonBody();
-
-$stmt = $pdo->prepare("UPDATE applications SET status = ? WHERE job_id = ? AND student_email = ?");
-$stmt->execute([$data["status"], $data["jobId"], $data["studentEmail"]]);
-echo json_encode(["success" => true]);
--- Project-2-main/api/job_delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-require "config.php";
-$data = readJsonBody();
-
-$stmt = $pdo->prepare("DELETE FROM jobs WHERE id = ?");
-$stmt->execute([$data["id"]]);
-echo json_encode(["success" => true]);
